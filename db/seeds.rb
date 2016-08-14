0bd9b957bc1a532c848b0dfd856280616584cc20
frankdew <frankdew23@gmail.com>
2016-08-14 09:12:40 +0200

    Add seeds and defined relation models

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Song.create( title: "Rio" )
+Song.create( title: "Relax" )
+Song.create( title: "Summer Of 69" )
+Song.create( title: "Gold" )
+Song.create( title: "Ghost Town" )
+Song.create( title: "Vienna" )
+
+Artist.create( name: "Duran Duran" )
+Artist.create( name: "Frankie Goes To Hollywood" )
+Artist.create( name: "Bryan Adams" )
+Artist.create( name: "Spandau Ballet" )
+Artist.create( name: "Specials" )
+Artist.create( name: "Ultravox" )
